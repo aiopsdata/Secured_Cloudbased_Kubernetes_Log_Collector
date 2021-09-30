70d06e589c65f480892a76a2235d5ffa5449edd5
aiopsdata <91112244+aiopsdata@users.noreply.github.com>
2021-09-30 17:55:22 +0530

    updated for kibana

--- uninstall.sh
+++ uninstall.sh
@@ -2,18 +2,19 @@
 log="0"
 metric="0"
 elastic="0"
-
-while getopts lme flag
+kibana="0"
+while getopts lmek flag
 do
     case "${flag}" in
        l) log="1";;
         m) metric="1";;
         e) elastic="1";;
+	k) kibana="1";;
     esac
 done
 
 
-if [[ $log == "1" || $metric == "1" || $elastic == "1" ]]
+if [[ $log == "1" || $metric == "1" || $elastic == "1" || $kibana == "1" ]]
 then
 	if [[ $log == "1" ]]
 	then 
@@ -34,13 +35,22 @@ then
 		fi
 	fi
 
+
+	if [[ $kibana == "1" ]]
+	then 
+		helm_list=$(helm ls -n aiops | awk '/kibana/{sub(/.kibana/, ""); print $1}')
+		if [[ $helm_list == "kibana" ]]
+		then
+			helm uninstall kibana -n aiops
+		fi
+	fi
 	
 	if [[ $elastic == "1" ]]
 	then 
-		helm_list=$(helm ls -n aiops | awk '/eskibana/{sub(/.eskibana/, ""); print $1}')
-		if [[ $helm_list == "database" ]]
+		helm_list=$(helm ls -n aiops | awk '/elastic/{sub(/.elastic/, ""); print $1}')
+		if [[ $helm_list == "elastic" ]]
 		then
-			helm uninstall database -n aiops
+			helm uninstall elastic -n aiops
 		fi
 	fi
 	
@@ -50,10 +60,9 @@ else
 	ar_len=${#helm_list[@]}
 	for (( i=1; i<$ar_len; i++))
 	do	
-		if [[ "${helm_list[$i]}" == "log" || "${helm_list[$i]}" == "metric" || "${helm_list[$i]}" == "database" || "${helm_list[$i]}" == "node-exporter" ]]
+		if [[ "${helm_list[$i]}" == "log" || "${helm_list[$i]}" == "metric" || "${helm_list[$i]}" == "elastic" || "${helm_list[$i]}" == "node-exporter" || "${helm_list[$i]}" == "kibana" ]]
 		then
 			helm uninstall ${helm_list[$i]} -n aiops
 		fi
 	done
 fi
-
